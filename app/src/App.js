d8c8bbc5b429d2b6fad99c3e98a120831dc286a8
Ameera Huda <ahuda6@gatech.edu>
2022-01-25 23:34:20 -0600

    Clean up code

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { StateProvider } from './contexts/state';
+import { Home } from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StateProvider>
+      <div className="App">
+        <Home></Home>
+      </div>
+    </StateProvider>
   );
 }
 
